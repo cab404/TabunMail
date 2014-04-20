1ffb21ec6a8f869b09afc98f2de52329fbe30a90
cab404 <cab404@ya.ru>
2014-04-20 20:20:39 +0400

    Initial

---
+++ src/everypony/tabunMail/activities/Main.java
@@ -0,0 +1,13 @@
+package everypony.tabunMail.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author cab404
+ */
+public class Main extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
